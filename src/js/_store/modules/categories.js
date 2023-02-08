eca6db46937690a128edab83ae21ae8c10808aa9
Onodera Yuki <ondryuki@gmail.com>
2023-02-08 17:45:59 +0900

    remove: 現時点で不要なコードの削除

--- src/js/_store/modules/categories.js
+++ src/js/_store/modules/categories.js
@@ -4,30 +4,16 @@ export default {
   namespaced: true,
   state: {
     categoryList: [],
-    categories: [],
     deleteCategoryId: null,
-    loading: false,
     doneMessage: '',
     errorMessage: '',
-  },
-  user: {
-    account_name: '',
-    created_at: '',
-    email: '',
-    full_name: '',
-    id: '',
-    password_reset_flg: null,
-    role: '',
-    updated_at: '',
   },
   mutations: {
     doneGetAllCategories(state, categories) {
       state.categoryList = categories.reverse();
-      state.loading = false;
     },
     failRequest(state, { message }) {
       state.errorMessage = `${message}が発生しています。ご確認の上、再度お試しください。`;
-      state.loading = false;
     },
   },
   actions: {
